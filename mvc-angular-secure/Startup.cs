077a516776b9e65c421cf898e76816aff9d568ec
Diego Wang <diegowang@yahoo.com>
2016-10-12 11:38:42 -0400

    Add project files.

---
+++ mvc-angular-secure/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(mvc_angular_secure.Startup))]
+namespace mvc_angular_secure
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
